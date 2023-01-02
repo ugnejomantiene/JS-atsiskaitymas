b78d869e3d0a01af3a64b7c099cb0c91da3ebb99
ugnejomantiene <upetkeviciute@gmail.com>
2023-01-02 11:37:08 +0200

    8 task done

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,32 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą;
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+class Calculator {
+    sum(a, b) {
+      return a + b;
+    }
+  
+    subtraction(a, b) {
+      return a - b;
+    }
+  
+    multiplication(a, b) {
+      return a * b;
+    }
+  
+    division(a, b) {
+      return a / b;
+    }
+  }
+
+const calculator = new Calculator();
+
+
+
+// ** Works **
+
+// console.log(calculator.sum(2, 3)); 
+// console.log(calculator.subtraction(2, 3)); 
+// console.log(calculator.multiplication(2, 3)); 
+// console.log(calculator.division(2, 3)); 
