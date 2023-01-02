c70fd5446de04a3fea1a317c69307161b5bc5091
ugnejomantiene <upetkeviciute@gmail.com>
2023-01-02 10:34:57 +0200

    4task. light styles

--- Task 4/script.js
+++ Task 4/script.js
@@ -10,3 +10,19 @@ bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'cars.json';
+
+
+fetch(ENDPOINT)
+  .then(info => info.json())
+  .then(data => data)
+  .then(data => {
+    let atvaizdavimas = ""
+    data.forEach(marke => {
+      atvaizdavimas += `
+  <div class="card">
+    <h1 class="brand">${marke.brand}</h1><br>
+    <p class="models">${marke.models}</p>
+  </div>
+  `
+    document.querySelector("#output").innerHTML = atvaizdavimas
+    })});
